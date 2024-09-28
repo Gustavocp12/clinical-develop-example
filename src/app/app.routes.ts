738caffae13b70059bbf381fa2ee6554647e39af
gustavocosta <gustavocp965@gmail.com>
2024-09-28 16:12:30 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import {authGuard} from "./core/guards/auth.guard";
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'consultas',
+    loadChildren: () => import('./pages/consultation/consultation.module').then(m => m.ConsultationModule),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'agendamento',
+    loadChildren: () => import('./pages/scheduling/scheduling.module').then(m => m.SchedulingModule),
+    canActivate: [authGuard]
+  }
+];
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {authGuard} from "./guards/auth.guard";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+})
+export class CoreModule { }
